f1c65bb16f84f408982dbcd8fc2752dcc5f7b498
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-03-03 17:53:34 +0100

    Update create_terrain.ts

--- src/Terrain/create_terrain.ts
+++ src/Terrain/create_terrain.ts
@@ -6,22 +6,16 @@ import { Terrain, TerrainConfig } from './terrain';
  * @returns {Terrain} Une instance de Terrain générée.
  */
 export function createTerrain(): Terrain {
-    // Définition de la configuration du terrain.
     const config: TerrainConfig = {
         rows: 6,               // Nombre de lignes du terrain.
         cols: 6,               // Nombre de colonnes du terrain.
         wallProbability: 0.3,  // Probabilité d'avoir un mur sur un côté (30%).
         contentProbabilities: {
-            // Définition de la distribution des contenus.
-            // Ici, le terrain aura 50% de chance d'être Vide,
-            // 30% de chance de contenir un Monstre,
-            // et 20% de chance de contenir un Tresor.
             'Vide': 0.5,
             'Monstre': 0.3,
             'Tresor': 0.2
         }
     };
 
-    // Création et retour du terrain généré selon la configuration.
     return new Terrain(config);
 }
