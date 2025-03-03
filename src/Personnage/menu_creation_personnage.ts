b87ac4204de55e477bcb1e0bcbb6a406a60af8bb
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-03-03 16:33:03 +0100

    ✨ Menu creation d'un personnage

---
+++ src/Personnage/menu_creation_personnage.ts
@@ -0,0 +1,40 @@
+import readline from "readline";
+import { Guerrier, Mage, Voleur, PersonnageConstructor } from "../Personnage/personnage";
+
+
+const classOptions: { [key: string]: { name: string; constructeur: PersonnageConstructor } } = {
+    "1": { name: "Guerrier", constructeur: Guerrier },
+    "2": { name: "Mage", constructeur: Mage },
+    "3": { name: "Voleur", constructeur: Voleur },
+};
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Entrez le nom de votre personnage (3-20 caractères) : ", (nom: string) => {
+    console.log("\nChoisissez la classe du personnage :");
+
+    for (const key in classOptions) {
+        if (classOptions.hasOwnProperty(key)) {
+            console.log(`${key}. ${classOptions[key].name}`);
+        }
+    }
+
+
+    rl.question("Votre choix : ", (choix: string) => {
+        let personnage;
+        const option = classOptions[choix];
+        if (option) {
+            personnage = new option.constructeur(nom);
+        } else {
+            console.log("Choix invalide, création d'un Guerrier par défaut.");
+            personnage = new Guerrier(nom);
+        }
+
+        console.log("\nPersonnage créé :");
+        console.log(personnage.afficherInfos());
+        rl.close();
+    });
+});
