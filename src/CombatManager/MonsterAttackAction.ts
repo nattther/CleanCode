53e36dcc0415b672e0878c7fc72da97b8eb5a29a
Nathan Tournant <115212705+nattther@users.noreply.github.com>
2025-03-27 14:45:45 +0100

    Update MonsterAttackAction.ts

--- src/CombatManager/MonsterAttackAction.ts
+++ src/CombatManager/MonsterAttackAction.ts
@@ -1,17 +1,9 @@
 import { CombatManager } from "./CombatManager";
 import { IPlayerAction } from "./IPlayerAction";
 
-/**
- * Classe représentant l'action d'attaque du monstre.
- */
 export class MonsterAttackAction implements IPlayerAction {
-  /**
-   * Exécute l'attaque du monstre en appliquant un modificateur de dégâts.
-   *
-   * @param combatManager Le gestionnaire de combat en cours.
-   * @param modifier Modificateur des dégâts (1 par défaut, 0.5 si le joueur se défend).
-   * @return Une chaîne de caractères décrivant l'attaque du monstre.
-   */
+
+
   execute(combatManager: CombatManager, modifier: number = 1): string {
     const player = combatManager.getPlayer();
     const monster = combatManager.getMonster();
